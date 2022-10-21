378efcc18d9e1b3ff832687225bbca8500d092a8
MarlonHersal <hern.sal.dz@gmail.com>
2022-10-20 22:12:58 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Nav from "./containers/Nav.jsx";
+
+export default function App(props) {
+  return (
+    <div className="App">
+      <Nav/>
+    </div>
+  );
+};
+
---
+++ src/containers/Nav.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { ReactDOM } from "react";
+import { render } from "react-dom";
+
+export default function Nav(){
+    return (
+        <div>
+            <div>Logo</div>
+            <p>Inicio</p>
+            <div>Buscador</div>
+            <p>Desarrollador</p>
+        </div>
+    )
+}
